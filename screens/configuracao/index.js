64926e8478441d202a1693b98623fbf2aa28e2b6
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2024-09-08 11:23:12 -0300

    opção de compartilhar link no whatsapp

--- screens/configuracao/index.js
+++ screens/configuracao/index.js
@@ -1,9 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Pressable, Linking, Alert, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
+  const shareViaWhatsApp = () => {
+    const url = 'https://www.google.com'; // URL a ser compartilhada
+    const message = `Olá pessoal, este é o meu site para realizar os agendamentos! ${url}`;
+    const whatsappUrl = `whatsapp://send?text=${encodeURIComponent(message)}`;
+
+    Linking.openURL(whatsappUrl)
+      .catch(() => {
+        Alert.alert('Erro', 'WhatsApp não está instalado');
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Configurações</Text>
+    <View>
+      <Pressable style={styles.btnShare} onPress={shareViaWhatsApp}>
+        <Text style={styles.btnShareText}>
+          Compartilhar no WhatsApp <Icon name="share-square-o" size={18} color="#fff" />
+        </Text>
+      </Pressable>
     </View>
   );
 }
@@ -11,8 +28,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 16,
+  },
+  btnShare: {
+    backgroundColor: '#c74098',
+    padding: 10,
+    borderRadius: 5,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  btnShareText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
